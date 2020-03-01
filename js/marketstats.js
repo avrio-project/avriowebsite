2ae69a6fbaca352943f7cf092c157d026741917d
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-03-01 12:17:01 +0000

    Update marketstats.js

--- js/marketstats.js
+++ js/marketstats.js
@@ -11,7 +11,7 @@ function getAvex() {
   let res = response.json();
   let btcrawprice = fetch("https://blockchain.info/ticker");
   let btcprice = btcraw.json().USD.buy;
-  let array = ["avex", res.price, res.volume_primary, res.low, res.high, '<iframe id="avex-chart" src='https://avex.exchange/chart/?primary=aio&secondary=btc" style="height:380px;width:100%"></iframe>', btcprice];
+  let array = ["avex", res.price, res.volume_primary, res.low, res.high, '<iframe id="avex-chart" src=\'https://avex.exchange/chart/?primary=aio&secondary=btc" style="height:380px;width:100%\'></iframe>', btcprice];
   return array;
 }
 window.onload(getMarketStats());
